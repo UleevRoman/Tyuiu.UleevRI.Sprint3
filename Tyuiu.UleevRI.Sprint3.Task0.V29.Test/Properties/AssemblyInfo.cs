26d33a3bd6b164a3c99b7a8a27865da68cb1ccbf
Tyuiu.UleevRI@outlook.com <romanuleev178@gmail.com>
2023-10-16 20:39:25 +0500

    Add project files.

---
+++ Tyuiu.UleevRI.Sprint3.Task0.V29.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.UleevRI.Sprint3.Task0.V29.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.UleevRI.Sprint3.Task0.V29.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d910ef0f-e3fb-4e09-867f-0f5802a6da2c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
